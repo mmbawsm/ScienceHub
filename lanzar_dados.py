638846968f8904fa47ce527cab67645ac593e0f8
Milagros Paniagua <mila.paniagua@gmail.com>
2024-06-27 08:35:07 +0000

    Revert "Cambiar el mensaje de salida en la función de lanzamiento de dados"
    This reverts commit 4f8e03a7187f2e1aa2c2a6ea8ee285f9c9daaf9a.

--- lanzar_dados.py
+++ lanzar_dados.py
@@ -6,10 +6,9 @@ def lanzar_dados(amount, sides):
     for i in range(amount):
         resultado = random.randint(1, sides)
         resultados.append(resultado)
-        print(f"Lanzamiento {i+1}: el número obtenido es {resultado}")
+        print(f"Lanzamiento {i+1} número obtenido {resultado}")
         time.sleep(5)
     return resultados
 
 if __name__ == "__main__":
     lanzar_dados(5, 6)
-
